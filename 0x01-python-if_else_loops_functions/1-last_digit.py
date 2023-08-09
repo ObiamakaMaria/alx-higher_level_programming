e4bd32e938e632711c1335bdee800171d5cf7a61
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-09 12:06:27 +0000

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,15 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10  # Calculate the absolute value of the last digit
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
 
-print("Last digit of {} is".format(number), end=" ")
+print("Last digit of {} is {} and is".format(number, digit), end=" ")
 
-if last_digit > 5:
-    print("{} and is greater than 5".format(last_digit))
-elif last_digit == 0:
-    print("{} and is 0".format(last_digit))
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-    print("{} and is less than 6 and not 0".format(last_digit))
+    print("less than 6 and not 0")
