b0a1bc92d04e8014d4b399a3f75e67f5e76e54fe
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-09 03:04:03 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,6 +6,6 @@ The two digits must be different - 01 and 10 are considered identical.
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
         if digit1 == 8 and digit2 == 9:
-            print(f"{digit1}{digit2}")
+            print("%d%d" % (digit1, digit2))
         else:
-            print(f"{digit1}{digit2}", end=", ")
+            print("%d%d" % (digit1, digit2), end=", ")
