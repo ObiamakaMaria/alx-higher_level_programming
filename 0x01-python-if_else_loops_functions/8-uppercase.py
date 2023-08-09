31ba2dd518384f454f9291003dba69b7b2a6f43f
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-09 03:53:50 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 8-uppercase.py
-
 
 def uppercase(str):
     """Print a string in uppercase."""
@@ -9,4 +7,6 @@ def uppercase(str):
                     c = chr(ord(c) - 32)
                                                 print("{}".format(c), end="")
                                                     print("")
+
+
 
