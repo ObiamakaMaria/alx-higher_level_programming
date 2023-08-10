64bbdc1140b57bc8fe03adaf41c377e62f94108c
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-10 10:34:35 +0000

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    """This function actually print the last digit of a number and return it."""
+    """This function actually print the last digit of a no & return it."""
     print(abs(number) % 10, end="")
     return (abs(number) % 10)
