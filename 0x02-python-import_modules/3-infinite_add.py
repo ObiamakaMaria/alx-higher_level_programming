8d932103cd0ae74f5e735fd027dbb4d787b471eb
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-11 12:51:33 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     import sys
-    #This helps us to acess the command line arguements 
+
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
