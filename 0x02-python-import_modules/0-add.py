a4b9bb0e2bf32fb9715166c6cee590a6ff05ee8f
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-11 11:41:15 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-# Defining  the variables
-    a = 1
-    b = 2
-
     """The script prints the sum of 1 and 2."""
     from add_0 import add
+    # Defining  the variables
+
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
