d10de26766c1778a553cb08020ee55f331ef6ebd
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-11 14:22:11 +0000

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """This script performs basic arithmetic operations."""
+    """The script performs basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div 
     import sys
 
