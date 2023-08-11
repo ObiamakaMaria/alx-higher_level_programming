9e99a5cf19f4141ea689c02ffb265b6562a75601
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-11 12:19:15 +0000

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     """Printing the +,-,* and quotient of 10 and 5"""
     from calculator_1 import add, sub, mul, div
-    #Defining the variables
+    
     a = 10
     b = 5
 
