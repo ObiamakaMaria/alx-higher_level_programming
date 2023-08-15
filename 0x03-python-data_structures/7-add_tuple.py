fe87e5eab8b26d6c728454720c4af3665bbe60d5
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-15 01:16:59 +0100

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    """This script returns  a tuple with 2 integers."""
+    """This script returns a tuple """
     if len(tuple_a) < 2:
         tuple_a += (0,) * (2 - len(tuple_a))
     if len(tuple_b) < 2:
