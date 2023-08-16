1587a0091cbb99a3082ae3409ee0afb78fb08c8f
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-16 08:15:42 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
     new_matrix = []  # Initialize an empty list to store the new matrix
     
@@ -8,5 +7,4 @@ def square_matrix_simple(matrix=[]):
         for value in row:
             new_row.append(value ** 2)
         new_matrix.append(new_row)
-    
     return new_matrix 
