0e2780588e53f2d831a77038efd764c1cf181114
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-16 08:50:58 -0700

    4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    diff_set = set()  #This empty set to store the differnce b/w the two set
+
+    for element in set_1:
+        if element not in set_2:
+            diff_set.add(element)
+
+    for element in set_2:
+        if element not in set_1:
+            diff_set.add(element)
+    return diff_set
