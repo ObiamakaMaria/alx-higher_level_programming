d81dfd8b16b75c4835265a4c87c62a6186df1f48
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-16 14:29:32 -0700

    11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-multiply_list_map = lambda my_list, number: list(map(lambda a: a * number, my_list))
-
-my_list = [1, 2, 3, 4, 6]
-new_list = multiply_list_map(my_list, 4)
-print(new_list)
-print(my_list)
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
