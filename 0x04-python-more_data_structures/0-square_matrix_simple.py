c18084fa8a05098c6fe27882910fa4ddb91f818c
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-17 13:24:58 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []  # Initialize an empty list to store the new matrix
-    
+
     for row in matrix:
         new_row = []  # This empty list  will store the new row
         for value in row:
             new_row.append(value ** 2)
         new_matrix.append(new_row)
-    return new_matrix 
+    return new_matrix
