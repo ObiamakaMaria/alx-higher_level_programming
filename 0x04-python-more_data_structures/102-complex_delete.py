b7a1209f8886975d57fce78f62c9e697e596b9cb
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-17 13:46:50 -0700

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -11,7 +11,3 @@ def complex_delete(a_dictionary, value):
         del a_dictionary[key]
 
     return a_dictionary
-
-def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
-        print("{}: {}".format(key, a_dictionary[key]))
