b1b9c4c8515e01d57574da4336d91cb2abf9a331
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-29 22:23:54 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This script defines a class Square."""
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size):
+        """Initializing  a new Square.
+
+        Args:
+            size (int): The size is typecasted to int..
+        """
+        self.__size = size
