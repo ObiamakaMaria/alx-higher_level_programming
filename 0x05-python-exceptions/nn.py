1156a0cd7cda8f1db39b67098c476c5e5cd6be18
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-30 23:21:21 +0100

    nn.py

--- 0x05-python-exceptions/nn.py
+++ 0x05-python-exceptions/nn.py
@@ -14,9 +14,9 @@ def safe_print_list(my_list=[], x=0):
     printed_count = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end=" ")
+            print("{}".format(my_list[i]), end="")
             printed_count += 1
         except IndexError:
             break
-    print()
-    return printed_count
+    print("")
+    return (printed_count)
