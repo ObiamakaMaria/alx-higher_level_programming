8ebc7e532762af5e16b1e0e4d4bbd4408cee8fa3
ObiamakaMaria <obiamaka0101@gmail.com>
2023-08-30 20:20:36 +0000

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-ef safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):
     """Prints up to x elements from my_list and returns the actual number of printed elements."""
     try:
         count = 0
