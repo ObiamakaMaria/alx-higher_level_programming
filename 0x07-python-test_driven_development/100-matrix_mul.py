00042c9a6762aa2d18eb1d680131de852ea29746
ObiamakaMaria <obiamaka0101@gmail.com>
2023-09-06 06:59:03 -0700

    100-matrix_mul.pyt

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -18,7 +18,8 @@ def matrix_mul(m_a, m_b):
     Return:
         The resulting matrix as a list of lists.
     """
-     if m_a == [] or m_a == [[]]:
+
+    if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
