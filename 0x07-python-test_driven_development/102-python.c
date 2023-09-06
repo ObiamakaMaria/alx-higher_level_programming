d3cfbf3ae359a8561d419d24f0a99297de6eed8a
ObiamakaMaria <obiamaka0101@gmail.com>
2023-09-06 12:34:15 +0000

    102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -13,41 +13,23 @@
  */
 void print_python_string(PyObject *p)
 {
-	if (!PyUnicode_Check(p))
+	long int length;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
-		fprintf(stderr, "[ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	Py_ssize_t length = PyUnicode_GET_LENGTH(p);
-	const char *type = PyUnicode_IS_COMPACT_ASCII(p) ?
-		"compact ascii" :
-		"compact unicode";
-	const char *value = PyUnicode_AsUTF8(p);
+	length = ((PyASCIIObject *)(p))->length;
 
-	fprintf(stdout, "[.] string object info\n");
-	fprintf(stdout, "  type: %s\n", type);
-	fprintf(stdout, "  length: %zd\n", length);
-	fprintf(stdout, "  value: %s\n", value);
-}
-/**
-* main - Entry point of the program.
-* This function initializes the Python interpreter, performs a series of
-* test cases using the print_python_string function, and finalizes the
-* Python interpreter.
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-	PyObject *s;
-
-	Py_Initialize();
-
-	s = PyUnicode_DecodeUTF8("The spoon does not exist", 24, "strict");
-	print_python_string(s);
-	Py_DECREF(s);
-
-	Py_Finalize();
-	return (0);
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
