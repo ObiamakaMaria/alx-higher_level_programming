72322c62deebd57feb182402b55211ba1a4b062f
ObiamakaMaria <obiamaka0101@gmail.com>
2023-09-06 05:04:17 -0700

    102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,43 @@
+#include <Python.h>
+
+/**
+ * print_python_string - This function prints information about a
+ * Python string object.
+ * @p: Pointer to the Python object to be printed.
+ *
+ * This function checks if the provided Python object is a valid
+ * string object, extracts its type, length, and value information,
+ * and prints it to the standard output.
+ * If the object is not a valid string, an error message is printed
+ * to the standard error.
+ */
+void print_python_string(PyObject *p)
+{
+    if (!PyUnicode_Check(p))
+    {
+        fprintf(stderr, "[ERROR] Invalid String Object\n");
+        return;
+    }
+
+    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
+    const char *type = PyUnicode_IS_COMPACT_ASCII(p) ? "compact ascii" : "compact unicode";
+    const char *value = PyUnicode_AsUTF8(p);
+
+    fprintf(stdout, "[.] string object info\n");
+    fprintf(stdout, "  type: %s\n", type);
+    fprintf(stdout, "  length: %zd\n", length);
+    fprintf(stdout, "  value: %s\n", value);
+}
+
+int main(void)
+{
+    PyObject *s;
+    Py_Initialize();
+
+    s = PyUnicode_DecodeUTF8("The spoon does not exist", 24, "strict");
+    print_python_string(s);
+    Py_DECREF(s);
+
+    Py_Finalize();
+    return (0);
+}
