6ae1afe4c3a6e197cceda96bdb93587b40d3a5bb
ObiamakaMaria <obiamaka0101@gmail.com>
2023-09-12 07:35:43 -0700

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This load python from json file string"""
+import json
+
+
+def load_from_json_file(filename):
+    """Loading  py data structure from file string
+
+    Args:
+        filename: The file name
+
+    Returns:
+        The python data structure
+    """
+    with open(filename, 'r') as myfile:
+        return (json.load(myfile))
