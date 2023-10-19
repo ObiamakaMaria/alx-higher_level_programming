d27e93549cb22f08e0cb4f78838082767c7e5c91
ObiamakaMaria <obiamaka0101@gmail.com>
2023-10-18 19:05:35 -0700

    9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- This lists all cities in the database.
+-- It sortes ascending order of cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
