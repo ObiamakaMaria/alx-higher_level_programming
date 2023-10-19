7988ccc6252cf7a65c54fea4b8f600be310b7f8c
ObiamakaMaria <obiamaka0101@gmail.com>
2023-10-18 18:59:51 -0700

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- This creates a user and grant him all permission
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost';
