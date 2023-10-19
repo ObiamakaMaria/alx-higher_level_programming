2017e32aaf4a14a14c2299260b1d12fe2828b923
ObiamakaMaria <obiamaka0101@gmail.com>
2023-10-18 19:04:15 -0700

    7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- This creates a database and table with pk
+-- It implements the use of NOT NULL Keyword.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities` (`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, `state_id` INT NOT NULL, `name` VARCHAR(256) NOT NULL, FOREIGN KEY(`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));
