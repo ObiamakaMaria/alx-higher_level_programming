1898dbf39eb2355c1fa8d125a14c0a43510bd591
ObiamakaMaria <obiamaka0101@gmail.com>
2023-10-18 19:07:16 -0700

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- This list shows every show with their genre
+SELECT s.`title`, g.`genre_id` FROM `tv_shows` AS s LEFT JOIN `tv_show_genres` AS g ON s.`id` = g.`show_id` ORDER BY s.`title`, g.`genre_id`;
