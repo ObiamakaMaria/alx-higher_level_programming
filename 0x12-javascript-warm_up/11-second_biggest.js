b0ede9a9ca69e460580a25723dd1064b203aa422
ObiamakaMaria <obiamaka0101@gmail.com>
2023-11-13 14:11:14 -0800

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,7 @@ const args = process.argv.slice(2).map(Number);
 if (args.length <= 1) {
   console.log(0);
 } else {
-
   const sortedUniqueArgs = Array.from(new Set(args)).sort((a, b) => b - a);
-  
+
   console.log(sortedUniqueArgs[1]);
 }
