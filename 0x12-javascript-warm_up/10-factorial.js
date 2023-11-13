06bc41fb4d476608474a3da28235bc78b783c959
ObiamakaMaria <obiamaka0101@gmail.com>
2023-11-13 14:02:42 -0800

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+function factorial(n) {
+
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+
+    return n * factorial(n - 1);
+  }
+}
+
+const input = parseInt(process.argv[2], 10);
+
+if (!isNaN(input)) {
+  console.log(factorial(input));
+} else {
+  console.log('1');
+}
