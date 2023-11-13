f8dc006034bd6315590f19fe36fe9138f9b5f409
ObiamakaMaria <obiamaka0101@gmail.com>
2023-11-13 13:26:36 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const values = [
-  "C is fun",
-  "Python is cool",
-  "JavaScript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (let a = 0; a < values.length; a++) {
