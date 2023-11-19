672331cfb40e73e6867f390b5a8dfcbd15b9cda6
ObiamakaMaria <obiamaka0101@gmail.com>
2023-11-19 14:01:27 -0800

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script prints the State objects from
+the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    db_session = Session()
+
+    query = db_session.query(State.id).filter(State.name == sys.argv[4]).first()
+    if query is None:
+        print('Not found')
+    else:
+        print(query[0])
