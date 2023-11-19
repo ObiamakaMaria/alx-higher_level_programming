51161e3c0be4bf94b516c9f2697c1dc9904ccbc7
ObiamakaMaria <obiamaka0101@gmail.com>
2023-11-19 12:42:58 -0800

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This script  all State objects that contain the letter a 
+""" This script  all State objects that contain the letter a
 from the database hbtn_0e_6_usa"""
 
 from model_state import State
