8c2cad0199d47dfd1014edc1f504614f04abec1e
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-01 06:12:10 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,3 +11,8 @@ if __name__ == "__main__":
     with request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
+
+        print("Body response:$")
+        print("    - type: {}".format(type(content)), "$")
+        print("    - content: {}".format(content), "$")
+        print("    - utf8 content: {}".format(utf8_content), "$")
