e3a8bf66dbafd0a4d492bb24574ba99038faf95a
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-01 18:02:45 -0800

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" This script sends POST request with an email as prarameter"""
+"""POST an email using requests"""
 
 if __name__ == "__main__":
     import requests
     import sys
-    c_option = sys.argv
 
-    param = {'email': c_option[2]}
-    resp = requests.post(c_option[1], param=param)
-    print(resp.text)
+    data = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=data)
+    print(r.text)
