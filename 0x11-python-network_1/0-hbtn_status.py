b94729cc3f66916aa18416f337be15885ca30da3
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-01 19:53:44 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """ This file gets the content of the URL given programmatically """
 
-
 if __name__ == "__main__":
     from urllib import request
-
     url = "https://alx-intranet.hbtn.io/status"
 
     with request.urlopen(url) as resp:
         content = resp.read()
-        utf8_content = content.decode('utf-8')
 
         print("Body response:$")
         print("    - type: {}".format(type(content)), "$")
