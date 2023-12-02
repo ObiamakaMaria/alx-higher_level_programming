03daae9a94b7af1e0b975ec0d599ab89ebea2b5d
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-01 17:10:06 -0800

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    This script displays the value of the variable X-Request-Id 
+    in the response header.
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+     if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+
+    url = sys.argv[1]
+
+    try:
+        resp = requests.get(url)
+
+        if 'X-Request-Id' in response.headers:
+            x_request_id = resp.headers['X-Request-Id']
+            print(x_request_id)
+        else:
+            print("X-Request-Id not found in the response headers.")
+
+    except requests.RequestException as e:
+        print("Error:", e)
+        sys.exit(1)
