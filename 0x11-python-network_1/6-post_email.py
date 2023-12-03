e2c7e33a21f1a0653692ec2861d81a74fa3b0834
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-02 18:36:44 -0800

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    info = {'email': sys.argv[2]}
-    resp = requests.post(sys.argv[1], info=info)
+    data = {'email': sys.argv[2]}
+    resp = requests.post(sys.argv[1], data=data)
     print(resp.text)
