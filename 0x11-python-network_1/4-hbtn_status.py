661baf93bf735a112ddaefe6c34e2d005bbdaac2
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-02 18:16:57 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 """
-This script GETS the specified URL & displays the response.
-"""
+This script GETS the specified URL & displays the response."""
 
 if __name__ == "__main__":
     import requests
-    import sys
+
     url = "https://alx-intranet.hbtn.io/status"
-
-    try:
-        resp = requests.get(url)
-
-        print("Body response:")
-        print("    - type: {}".format(type(resp.text)))
-        print("    - content: {}".format(resp.text))
-
-    except requests.RequestException as e:
-        print("Error:", e)
-        sys.exit(1)
+    resp = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
