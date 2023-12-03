39710e770031fbb22241f50c93eb244fef2d5505
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-02 19:08:47 -0800

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""This script uses the GitHub API to display your user id."""
-if__name__ == "__main__":
+"""get properties from mail"""
 
+if __name__ == '__main__':
     import requests
     import sys
 
     headers = {}
     headers['Authorization'] = '{} {}'.format(sys.argv[0], sys.argv[1])
-    re = requests.get('https://api.github.com/users/{}'
+    r = requests.get('https://api.github.com/users/{}'
                      .format(sys.argv[1]), headers=headers)
-    print(re.json().get('id'))
+    print(r.json().get('id'))
