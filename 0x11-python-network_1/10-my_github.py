807d5bbd8f40b46a3f1175b753f4387a0576f043
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-02 19:12:34 -0800

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     import sys
     headers = {}
     headers['Authorization'] = '{} {}'.format(sys.argv[0], sys.argv[1])
-    re = requests.get('https://api.github.com/users/{}'
+    e = requests.get('https://api.github.com/users/{}'
                      .format(sys.argv[1]), headers=headers)
-    print(re.json().get('id'))
+    print(e.json().get('id'))
